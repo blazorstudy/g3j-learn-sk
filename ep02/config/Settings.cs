584f261d05d19a4b679e5c2c9de160751bf9c42d
Jongin Lee <jongin@designme.ai>
2024-11-05 21:44:35 +0900

    ep02 데모 2 추가 (#2)
    EP02/Demo 2:
    
    - LLM 갈아 끼우기
    - SK 인스턴스 여러개에 각각 LLM 붙이기
    - SK 인스턴스 하나에 여러 LLM 붙이기

---
+++ ep02/config/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+public class Settings
+{
+    public string AzureOpenAIEndpoint { get; set; }
+    public string AzureOpenAIApiKey { get; set; }
+    public string GoogleGeminiKey { get; set; }
+    public string HuggingFacePhiApiKey { get; set; }
+
+    public static Settings LoadFromFile()
+    {
+        string filePath = "config/AppSetting.json";
+        string json = System.IO.File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<Settings>(json);
+    }
+
+
+}
