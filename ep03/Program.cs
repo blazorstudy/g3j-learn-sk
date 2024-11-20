225d34094a8ce1dbcc6112b499a90d4206c6f27b
Gusam Park <tnara9573@gmail.com>
2024-11-21 00:31:13 +0900

    Add ep03 데모 1

---
+++ ep03/Program.cs
@@ -0,0 +1,18 @@
+﻿#pragma warning disable SKEXP0050
+using Microsoft.SemanticKernel;
+
+var yourDeploymentName = "";
+var yourEndpoint = "";
+var yourApiKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+    deploymentName: yourDeploymentName,
+    endpoint: yourEndpoint,
+    apiKey: yourApiKey);
+
+var kernel = builder.Build();
+var result = await kernel.InvokePromptAsync(
+    "Give me a list of breakfast foods with eggs and cheese");
+
+Console.WriteLine(result);
--- ep03/README.md
+++ ep03/README.md
@@ -1,3 +1,5 @@
-# EP03: 
+# EP03: Semantic Kernel 플러그인 만들기
 
-TBD
+## Demo #1 - 프롬프트 플러그인 만들기
+
+## Demo #2 - 네이티브 플러그인 만들기
