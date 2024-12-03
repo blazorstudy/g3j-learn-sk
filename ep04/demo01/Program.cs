20ce1a1c14d704dc86ca4827bab81eab7e36fb0c
김 진석(Kim, JinSeok) <iamjinseok@gmail.com>
2024-12-03 23:55:49 +0900

    챗히스토리, 페르소나 샘플 추가

---
+++ ep04/demo01/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using demo01;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+
+#region Configuration
+IConfiguration config = new ConfigurationBuilder()
+                                .AddUserSecrets<Program>()
+                                .Build();
+
+string deployName = config["AOAI:DeployName"];
+string endPoint = config["AOAI:EndPoint"];
+string key = config["AOAI:Key"];
+string model = config["AOAI:Model"];
+#endregion
+
+var kernel = Kernel.CreateBuilder()
+            .AddAzureOpenAIChatCompletion(deployName, endPoint, key, model)
+            .Build();
+
+Console.WriteLine("Hello, Semantic Kernel!");
+
+// 챗컴플리션???
+// Chat Completion : Generate a response given a list of messages in a conversational context
+// 주어진 메시지 목록(맥락이 있는)에서 답변을 생성하는 것
+
+// Chat History
+// System Message : 사용자가 변경할 수 없는 유용한 정보를 담는 곳. 페르소나, 그라운딩, 세이프티 메시지 등
+// User Message : 사용자가 입력한 메시지
+// Asisstant Message : LLM이 답변한 메시지
+
+// 샘플 1 : 개요, 비스트리밍 챗 컴플리션
+Console.WriteLine("\nSample1");
+await Sample.showOverview(kernel);
+
+// 샘플 2 : 챗히스토리 오프젝트, 스트리밍 챗 컴플리션
+Console.WriteLine("\nSample2");
+await Sample.showChatHistory(kernel);
+
+// 샘플 3 : 페르소나
+Console.WriteLine("\nSample3");
+await Sample.showPersonas(kernel);
---
+++ ep04/demo01/Sample.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+
+namespace demo01;
+
+public class Sample
+{
+    public static async Task showOverview(Kernel kernel)
+    {
+        var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+        ChatHistory history = [];
+        history.AddUserMessage("자기소개 부탁해");
+
+        // 비스트리밍 
+        var response = await chatService.GetChatMessageContentAsync(
+            history,
+            kernel: kernel
+        );
+        Console.WriteLine(response);
+    }
+
+    public static async Task showChatHistory(Kernel kernel)
+    {
+        // 조금 더 리치한 거
+        var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+        ChatHistory history = [];
+        history.AddUserMessage("내가 좋아하는 스포츠는 축구야");
+        history.AddAssistantMessage("축구를 좋아하시는군요. 어느 팀을 좋아하시나요?");
+        history.Add(
+                    new()
+                    {
+                        Role = AuthorRole.User,
+                        Items = [
+                            new TextContent { Text = "한국팀을 좋아해" },
+                            new TextContent { Text = "월드컵은 언제 열려?" }
+                        ]
+                    });
+
+        // 스트리밍 챗 컴플리션
+        var response = chatService.GetStreamingChatMessageContentsAsync(history, kernel: kernel);
+        await foreach (var chuck in response)
+        {
+            Console.WriteLine(chuck);
+        }
+    }
+
+    public static async Task showPersonas(Kernel kernel)
+    {
+        // 시스템 메시지 세팅
+        var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+        ChatHistory history = [];
+        history.AddSystemMessage("당신은 한국인이고 남성입니다. 프로그래머이고 취미는 게임하기 입니다. 답변은 단답형으로 합니다.");
+        history.AddUserMessage("자기소개 부탁해");
+
+        // 비스트리밍 
+        var response = await chatService.GetChatMessageContentAsync(
+            history,
+            kernel: kernel
+        );
+        Console.WriteLine(response);
+    }
+}
