35dcf6fafdc334de0fd5033ad68d989f107f7961
Justin Yoo <justin.yoojh@gmail.com>
2024-12-05 15:38:04 +0900

    Update demos for ossfestival

---
+++ ossfestival/Plugins/Appointment.cs
@@ -0,0 +1,7 @@
+public class Appointment
+{
+    public DateTimeOffset Start { get; set; }
+    public string? Restaurant { get; set; }
+    public int PartySize { get; set; }
+    public string? ReservationId { get; set; }
+}
