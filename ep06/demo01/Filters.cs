71f9d807ee7d1449d25438479c4e6434e08a0821
김 진석(Kim, JinSeok) <iamjinseok@gmail.com>
2025-01-01 22:07:05 +0900

    add filtering demo for ep6

---
+++ ep06/demo01/Filters.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace demo01;
+
+public class PromptFilter1 : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} PromptFilter1");
+        
+        await next(context);
+
+        context.RenderedPrompt = context.RenderedPrompt + "\n 한 문장으로 표현해줘.";
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} PromptFilter1 ${context.RenderedPrompt}\n");
+    }
+}
+
+public class PromptFilter2 : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} PromptFilter2");
+
+        await next(context);
+
+        context.RenderedPrompt = context.RenderedPrompt + "\n 한글로 표현해줘.";
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} PromptFilter2 ${context.RenderedPrompt}\n");
+    } 
+}
+
+public class FunctionFilter1 : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} FunctionFilter1");
+        
+        await next(context);
+
+        //Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} FunctionFilter1에서 답변을 변경합니다.");
+        //context.Result = new FunctionResult(context.Function, value: "G3J를 보는 것이에요!!!");
+    }
+}
+
+public class FunctionFilter2 : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} FunctionFilter2");
+
+        await next(context);
+
+        if (context.IsStreaming)
+        {
+            Console.WriteLine($"DateTime.Now.ToString(\"yyyy-MM-dd HH:mm:ss.fff\")}} streaming");
+        }
+        else
+        {
+            Console.WriteLine($"DateTime.Now.ToString(\"yyyy-MM-dd HH:mm:ss.fff\")}} non-streaming");
+        }
+        Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} FunctionFilter2 {context.Result}");
+    }
+}
---
+++ ep06/demo01/Program.cs
@@ -0,0 +1,48 @@
+﻿using demo01;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+
+#region Configuration
+IConfiguration config = new ConfigurationBuilder()
+                                .AddUserSecrets<Program>()
+                                .Build();
+
+string deployName = config["AOAI:DeployName"];
+string endPoint = config["AOAI:EndPoint"];
+string key = config["AOAI:Key"];
+string model = config["AOAI:Model"];
+#endregion
+
+/*
+ * 1. 필터란?
+ * 2. 필터의 종류
+ *      - PromptRenderFilter
+ *      - FunctionInvocationFilter
+ *      - AutoFunction Invocation Filter
+ * 3. Demo
+ */
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(deployName, endPoint, key, model);
+
+builder.Services.AddSingleton<IFunctionInvocationFilter, FunctionFilter1>();
+builder.Services.AddSingleton<IPromptRenderFilter, PromptFilter1>();
+builder.Services.AddSingleton<IPromptRenderFilter, PromptFilter2>();
+builder.Services.AddSingleton<IFunctionInvocationFilter, FunctionFilter2>();
+
+//  각 속성에 따로 넣으셔도 됩니다.
+//  kernel.PromptRenderFilters.Add(new PromptFilter1());
+//  kernel.PromptRenderFilters.Add(new PromptFilter2());
+//  kernel.FunctionInvocationFilters.Add(new FunctionFilter1());
+//  kernel.FunctionInvocationFilters.Add(new FunctionFilter2());
+
+var kernel = builder.Build();
+
+var response = await kernel.InvokePromptAsync("Explain Semantic Kernel filtering.");
+Console.WriteLine("\n응답:");
+Console.WriteLine(response);
+Console.WriteLine("\n");
