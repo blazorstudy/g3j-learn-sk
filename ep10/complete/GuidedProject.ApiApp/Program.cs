8b3ed6d47899fc59b21efc52a058bdfb78bf602a
Justin Yoo <justin.yoojh@gmail.com>
2025-02-20 12:34:37 +0900

    Update complete app

--- ep10/complete/GuidedProject.ApiApp/Program.cs
+++ ep10/complete/GuidedProject.ApiApp/Program.cs
@@ -1,5 +1,11 @@
+using System.ClientModel;
+
 using GuidedProject.ApiApp.Endpoints;
 using GuidedProject.ApiApp.Services;
+
+using Microsoft.SemanticKernel;
+
+using OpenAI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +15,24 @@ builder.AddServiceDefaults();
 builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IKernelService, KernelService>();
+
+builder.Services.AddSingleton<Kernel>(sp =>
+{
+    var config = builder.Configuration;
+
+    var client = new OpenAIClient(
+        credential: new ApiKeyCredential(config["GitHub:Models:AccessToken"]!),
+        options: new OpenAIClientOptions { Endpoint = new Uri(config["GitHub:Models:Endpoint"]!) });
+
+    var kernel = Kernel.CreateBuilder()
+                       .AddOpenAIChatCompletion(
+                           modelId: config["GitHub:Models:ModelId"]!,
+                           openAIClient: client,
+                           serviceId: "github")
+                       .Build();
+
+    return kernel;
+});
 
 var app = builder.Build();
 
--- ep10/complete/GuidedProject.ApiApp/Services/KernelService.cs
+++ ep10/complete/GuidedProject.ApiApp/Services/KernelService.cs
@@ -1,3 +1,5 @@
+using Microsoft.SemanticKernel;
+
 namespace GuidedProject.ApiApp.Services;
 
 public interface IKernelService
@@ -5,32 +7,18 @@ public interface IKernelService
     IAsyncEnumerable<string> CompleteChatStreamingAsync(string prompt);
 }
 
-public class KernelService : IKernelService
+public class KernelService(Kernel kernel, IConfiguration config) : IKernelService
 {
-    private readonly static string loremipsum = """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Vestibulum sed est nisi. Etiam scelerisque ligula nec nisl elementum,
-        ac gravida tellus vestibulum. Vestibulum sed euismod elit.
-        Phasellus condimentum mi vitae efficitur viverra.
-        Proin ac fermentum nunc.
-        Vestibulum in neque vulputate, tincidunt ex id, faucibus risus.
-        Sed viverra, sem a ultricies bibendum, velit sapien cursus quam,
-        ornare tristique tortor ante sed ipsum.
-        Donec consectetur, felis quis scelerisque varius, tortor neque hendrerit lacus,
-        eu vestibulum elit neque tempus lacus.
-        Donec vitae risus id diam volutpat ornare sit amet sed est.
-        Fusce cursus finibus leo quis commodo.
-        """;
-
     public async IAsyncEnumerable<string> CompleteChatStreamingAsync(string prompt)
     {
-        var result = loremipsum.Split([ " ", "\r", "\n" ], StringSplitOptions.RemoveEmptyEntries);
+        var settings = new PromptExecutionSettings { ServiceId = config["SemanticKernel:ServiceId"] };
+        var arguments = new KernelArguments(settings);
 
-        foreach (var text in result)
+        var result = kernel.InvokePromptStreamingAsync(prompt, arguments).ConfigureAwait(false);
+
+        await foreach (var text in result)
         {
-            yield return text;
+            yield return text.ToString();
         }
-
-        await Task.CompletedTask.ConfigureAwait(false);
     }
 }
